e00db333f85dd8020b5c56a80cc59e9f463c2491
Peter Son <peteson.me@gmail.com>
2021-07-22 22:41:55 +1200

    setup frontend react app

--- webpack.config.js
+++ webpack.config.js
@@ -17,8 +17,8 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        query: {
-          presets: ['es2015', 'react'],
+        options: {
+          presets: ['@babel/preset-env'],
         },
       },
     ],
