33a0f0043928f172f77af71f08debcba42c25550
Peter Son <peteson.me@gmail.com>
2021-07-29 00:00:04 +1200

    first try

--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -3,7 +3,7 @@ import IndexController from '@controllers/index.controller';
 import { Routes } from '@interfaces/routes.interface';
 
 class IndexRoute implements Routes {
-  public path = '/';
+  public path = '*';
   public router = Router();
   public indexController = new IndexController();
 
