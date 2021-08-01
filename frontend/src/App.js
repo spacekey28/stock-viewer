3a542279f41d01322c4d9b6e667028be724b77fe
Peter Son <peteson.me@gmail.com>
2021-08-02 00:14:39 +1200

    chart component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Chart from './components/Chart';
+
 class App extends Component {
   render() {
     return (
       <div>
-        <h1>Hello World</h1>
+        <Chart />
       </div>
     );
   }
 }
+
 export default App;
