ff059336145cc792dcc7f954879487e8c24af74f
Peter Son <peteson.me@gmail.com>
2021-08-04 23:58:36 +1200

    data arranged

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,36 @@ import React, { Component } from 'react';
 import Chart from './components/Chart';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      chartData: {},
+    };
+  }
+
+  componentWillMount() {
+    this.getChartData();
+  }
+
+  getChartData() {
+    this.setState({
+      chartData: {
+        labels: ['IBM', 'Apple', 'Amazon', 'Microsoft', 'NVidia', 'Nio', 'Tesla', 'Netflix', 'Shopify', 'Alphabet'],
+        datasets: [
+          {
+            label: 'Price',
+            data: [1010, 1083, 983, 849, 1283, 394, 923, 1003, 1011],
+            backgroundColor: ['red', 'blue', 'green', 'pink', 'black', 'orange', 'purple', 'yellow', 'white'],
+          },
+        ],
+      },
+    });
+  }
+
   render() {
     return (
       <div>
-        <Chart legendPosition="bottom" />
+        <Chart chartData={this.state.chartData} legendPosition="bottom" />
       </div>
     );
   }
--- frontend/src/components/Chart.js
+++ frontend/src/components/Chart.js
@@ -4,39 +4,7 @@ import { Bar, Line, Pie } from 'react-chartjs-2';
 class Chart extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      chartData: {
-        labels: ['IBM', 'Apple', 'Amazon', 'Microsoft', 'NVidia', 'Nio', 'Tesla', 'Netflix', 'Shopify', 'Alphabet'],
-        datasets: [
-          {
-            'Meta Data': {
-              '1. Information': 'Intraday (5min) open, high, low, close prices and volume',
-              '2. Symbol': 'IBM',
-              '3. Last Refreshed': '2021-07-30 19:10:00',
-              '4. Interval': '5min',
-              '5. Output Size': 'Compact',
-              '6. Time Zone': 'US/Eastern',
-            },
-            'Time Series (5min)': {
-              '2021-07-30 19:10:00': {
-                '1. open': '140.9800',
-                '2. high': '140.9800',
-                '3. low': '140.9800',
-                '4. close': '140.9800',
-                '5. volume': '527',
-              },
-              '2021-07-30 17:30:00': {
-                '1. open': '141.1900',
-                '2. high': '141.1900',
-                '3. low': '141.1900',
-                '4. close': '141.1900',
-                '5. volume': '300',
-              },
-            },
-          },
-        ],
-      },
-    };
+    this.state = props.chartData;
   }
 
   static defaultProps = {
