85c0525aa69d676dbb2b4044b1754aedc1b1b683
Peter Son <peteson.me@gmail.com>
2021-08-07 23:57:33 +1200

    background color added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
     };
   }
 
-  componentWillMount() {
+  componentDidMount() {
     // getting chart data
     this.getChartData();
   }
@@ -22,7 +22,23 @@ class App extends Component {
           {
             label: 'Price',
             data: [1010, 1083, 983, 849, 1283, 394, 923, 1003, 1011, 2020],
-            backgroundColor: ['red', 'blue', 'green', 'pink', 'black', 'orange', 'purple', 'yellow', 'white', 'lightblue'],
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)',
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+            ],
+            borderWidth: 1,
           },
         ],
       },
